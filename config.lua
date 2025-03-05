aa7bf505ce28318ff474466a8c96f59ee1acdf94
KaloudasDev <kaloudasdev@gmail.com>
2025-03-05 23:14:48 +0200

    Uploaded Folders

---
+++ config.lua
@@ -0,0 +1,41 @@
+Config = Config or {}
+
+Config.blipsShow = true
+
+Config.Locations = {
+[1] = {
+    vector = vector3(538.32, -183.18, 54.47), 
+    text = "Repair Station", 
+    color = 1, 
+    sprite = 446, 
+    scale = 0.5,
+},
+[2] = {
+    vector = vector3(-778.01, 4965.29, 209.68), 
+    text = "Hunting Zone", 
+    color = 5, 
+    sprite = 51, 
+    scale = 0.8,
+},
+[3] = {
+    vector = vector3(-72.05, 6252.36, 31.09),
+    text = "Chicken Factory", 
+    color = 26, 
+    sprite = 273, 
+    scale = 0.9,
+},
+[4] = {
+    vector = vector3(-598.839538, -930.092286, 23.854248),
+    text = "Weazel News", 
+    color = 1, 
+    sprite = 135, 
+    scale = 0.9,
+},
+[5] = {
+    vector = vector3(582.408814, -8.043956, 101.245362),
+    text = "Police Department", 
+    color = 38, 
+    sprite = 60, 
+    scale = 0.9,
+},
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Kaloudas'
+description 'A customizable blip creator for GTA 5 servers'
+version '1.0'
+discord 'https://discord.gg/noblecraft'
+scriptname 'noble-blips'
+
+shared_script 'config.lua'
+client_script 'noble-blips.lua'
